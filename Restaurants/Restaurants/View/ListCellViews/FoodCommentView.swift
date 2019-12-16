0cdc3e4ddd390f1948a5aeade468af90b7bb6bf9
ShyamZ-ios <shyamala06.ios@gmail.com>
2019-12-16 21:49:36 +0530

    Complete Restaurant UI integration

---
+++ Restaurants/Restaurants/View/ListCellViews/FoodCommentView.swift
@@ -0,0 +1,21 @@
+//
+//  FoodCommentView.swift
+//  Restaurants
+//
+//  Created by Abservetech on 11/12/19.
+//  Copyright © 2019 Abservetech. All rights reserved.
+//
+
+import SwiftUI
+
+struct FoodCommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FoodCommentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodCommentView()
+    }
+}
---
+++ Restaurants/Restaurants/View/OtherViews/OnboradPage/OnboardView.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardView.swift
+//  Restaurants
+//
+//  Created by Abservetech on 12/12/19.
+//  Copyright © 2019 Abservetech. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardView()
+    }
+}
